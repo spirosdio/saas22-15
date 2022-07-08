82b12973fe3226e0c82af02ac95621556cf67950
spirosspiros <spiros@fake.com>
2022-07-09 01:17:36 +0300

    getting closer to ready  front

--- my-app/src/NavBar.js
+++ my-app/src/NavBar.js
@@ -1,60 +1,27 @@
+import React from "react";
 
-
-import React from 'react'
-
-import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 export default function NavBar() {
+  return (
+    <>
+      <Navbar style={{ color: "black", margin: "5vh" }}>
+        <Container className="navigationBar">
+          <Nav className="me-auto">
+            <Nav.Link style={{ color: "black", margin: "5vh" }} href="/About">
+              About
+            </Nav.Link>
 
-    return (
+            <Nav.Link style={{ color: "black", margin: "5vh" }} href="/Plans">
+              Plans
+            </Nav.Link>
 
-        <>
-
-
-
-            <Navbar style={{ margin: "5vh" }}>
-
-                <Container className="navigationBar">
-
-
-
-                    <Nav className="me-auto" >
-
-                        <Nav.Link
-
-                            style={{ color: "white" , margin: "5vh"}} 
-
-                            href="/About">About</Nav.Link>
-
-
-
-                        <Nav.Link
-
-                            style={{ color: "white" , margin: "5vh"}} 
-
-                            href="/Plans">Plans</Nav.Link>
-
-
-
-                        <Nav.Link
-
-                            style={{ color: "white" , margin: "5vh"}} 
-
-                            href="/Legal">Legal</Nav.Link>
-
-                    </Nav>
-
-                </Container>
-
-            </Navbar>
-
-
-
-
-
-        </>
-
-    )
-
+            <Nav.Link style={{ color: "black", margin: "5vh" }} href="/Legal">
+              Legal
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
-
