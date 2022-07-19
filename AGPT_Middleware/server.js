731f5fd352a28491ec7502a0c7d305a71df12ef3
spirosspiros <spiros@fake.com>
2022-07-19 04:08:03 +0300

    new horizons is a functional front end , but frontend is not working fullybut looks good

--- AGPT_Middleware/server.js
+++ AGPT_Middleware/server.js
@@ -1,111 +1,143 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const router = express.Router();
-const Kafka = require('node-rdkafka');
+const Kafka = require("node-rdkafka");
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+
+app.use(cors());
 
 var global_client_id = 0;
 var global_data = {};
 
+const request_data_producer = Kafka.Producer.createWriteStream(
+  {
+    "metadata.broker.list": "localhost:9092",
+    "message.max.bytes": "104857600",
+  },
+  {},
+  { topic: "AGPT_request_data" }
+);
 
-const request_data_producer = Kafka.Producer.createWriteStream({
-    'metadata.broker.list': 'localhost:9092',
-    'message.max.bytes': '104857600'
-},{}, {topic: 'AGPT_request_data'});
+const debug_producer = Kafka.Producer.createWriteStream(
+  {
+    "metadata.broker.list": "localhost:9092",
+    "message.max.bytes": "104857600",
+  },
+  {},
+  { topic: "Debug_topic" }
+);
 
-const debug_producer = Kafka.Producer.createWriteStream({
-    'metadata.broker.list': 'localhost:9092',
-    'message.max.bytes': '104857600'
-},{}, {topic: 'Debug_topic'});
-
-const AGPTReplyConsumer = Kafka.KafkaConsumer({
-    'group.id': 'kafka',
-    'metadata.broker.list': 'localhost:9092',
-    'max.partition.fetch.bytes': '104857600',
-},
-{}, {});
+const AGPTReplyConsumer = Kafka.KafkaConsumer(
+  {
+    "group.id": "kafka",
+    "metadata.broker.list": "localhost:9092",
+    "max.partition.fetch.bytes": "104857600",
+  },
+  {},
+  {}
+);
 AGPTReplyConsumer.connect();
 
-AGPTReplyConsumer.on('ready', function() {
-    console.log('AGPTReplyConsumer is ready');
-    AGPTReplyConsumer.subscribe(['AGPT_reply_data']);
-    AGPTReplyConsumer.consume();
-}).on('event.error', function(err) {
-    console.log('Error from AGPTReplyConsumer');
+AGPTReplyConsumer.on("ready", function () {
+  console.log("AGPTReplyConsumer is ready");
+  AGPTReplyConsumer.subscribe(["AGPT_reply_data"]);
+  AGPTReplyConsumer.consume();
+})
+  .on("event.error", function (err) {
+    console.log("Error from AGPTReplyConsumer");
     console.log(err);
-}).on('data', function(data) {
+  })
+  .on("data", function (data) {
     var myobj = JSON.parse(data.value.toString());
-    
+
     global_data[myobj.client_id] = true;
-    global_data['data' + myobj.client_id] = myobj.data;
+    global_data["data" + myobj.client_id] = myobj.data;
+  });
 
-});
-
-
-function send_request(message){
-    request_data_producer.write(Buffer.from(JSON.stringify(message)), (err) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log('Message sent successfully');
-        }
-    })
-}
-
-function send_debug(message){
-    debug_producer.write(Buffer.from(JSON.stringify(message)), (err) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log('Message sent successfully');
-        }
-    })
-}
-
-app.use('/:dateFrom&:country&:ProductionType', (req, res) => {
-
-    var my_client_id = global_client_id;
-    global_client_id++;
-    var dateFrom = req.params.dateFrom;
-    dateFrom = new Date(dateFrom);
-    const country = req.params.country;
-    const ProductionType = req.params.ProductionType;
-    const request = { client_id: my_client_id, country: country, dateFrom: dateFrom, ProductionType: ProductionType };
-    send_request(request);
-    let debug = {
-        message: 'AGPT Middleware sent message to kafka requesting: ' + country + ' from ' + dateFrom + ' with production type' + ProductionType + ' and with client_id: ' + my_client_id,
-        data: request,
+function send_request(message) {
+  request_data_producer.write(Buffer.from(JSON.stringify(message)), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Message sent successfully");
     }
+  });
+}
+
+function send_debug(message) {
+  debug_producer.write(Buffer.from(JSON.stringify(message)), (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Message sent successfully");
+    }
+  });
+}
+
+app.use("/:dateFrom&:country&:ProductionType", (req, res) => {
+  var my_client_id = global_client_id;
+  global_client_id++;
+  var dateFrom = req.params.dateFrom;
+  dateFrom = new Date(dateFrom);
+  const country = req.params.country;
+  const ProductionType = req.params.ProductionType;
+  const request = {
+    client_id: my_client_id,
+    country: country,
+    dateFrom: dateFrom,
+    ProductionType: ProductionType,
+  };
+  send_request(request);
+  let debug = {
+    message:
+      "AGPT Middleware sent message to kafka requesting: " +
+      country +
+      " from " +
+      dateFrom +
+      " with production type" +
+      ProductionType +
+      " and with client_id: " +
+      my_client_id,
+    data: request,
+  };
+  send_debug(debug);
+  global_data[my_client_id.toString()] = false;
+  global_data["data" + my_client_id.toString()] = {};
+
+  let p = new Promise((resolve) => {
+    function checkFlag() {
+      if (global_data[my_client_id.toString()]) {
+        resolve();
+      } else {
+        setTimeout(checkFlag, 100);
+      }
+    }
+    checkFlag();
+  });
+
+  p.then(() => {
+    debug = {
+      message:
+        "AGPT Middleware received message from kafka: " +
+        country +
+        " from " +
+        dateFrom +
+        " with production type" +
+        ProductionType +
+        " and with client_id: " +
+        my_client_id,
+      data: global_data["data" + my_client_id.toString()],
+    };
     send_debug(debug);
-    global_data[my_client_id.toString()] = false;
-    global_data['data' + my_client_id.toString()] = {};
-
-    let p = new Promise(resolve => {
-        function checkFlag() {
-          if (global_data[my_client_id.toString()]) {
-            resolve();
-          } else {
-            setTimeout(checkFlag, 100); 
-          }
-        }
-        checkFlag();
-    });
-    
-    p.then(() => {
-        debug = {
-            message: 'AGPT Middleware received message from kafka: ' + country + ' from ' + dateFrom + ' with production type' + ProductionType + ' and with client_id: ' + my_client_id,
-            data: global_data['data' + my_client_id.toString()],
-        }
-        send_debug(debug);
-        res.status(200).json(global_data['data' + (my_client_id).toString()]);
-        delete global_data[my_client_id.toString()];
-        delete global_data['data' + my_client_id.toString()];
-    });
-
+    res.status(200).json(global_data["data" + my_client_id.toString()]);
+    delete global_data[my_client_id.toString()];
+    delete global_data["data" + my_client_id.toString()];
+  });
 });
 
-app.listen(process.env.AGPT_MIDDLEWARE_PORT, () => console.log('Listening on port ' + process.env.AGPT_MIDDLEWARE_PORT));
+app.listen(process.env.AGPT_MIDDLEWARE_PORT, () =>
+  console.log("Listening on port " + process.env.AGPT_MIDDLEWARE_PORT)
+);
---
+++ new-horizons/src/App.js
@@ -0,0 +1,350 @@
+import "./App.css";
+import MyChart from "./MyChart";
+import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+//////////////   my constants to be variables from avar////////////////////////////////////////////////////////////////
+
+const myTimeUrl = "http://localhost:3020";
+const ATL = "Actual Total Load";
+const AGPT = "Aggregate Generation per Type";
+const PF = "Physical Flows";
+const quantities = [ATL, AGPT, PF];
+const CountriesObject = {
+  Bulgaria: "BGCTY",
+  Greece: "GRCTY",
+  Italy: "ITCTY",
+  "North Macedonia": "MKCTY",
+  Turkey: "TRCTY",
+};
+const typesObject = {
+  Biomass: "Biomass",
+  "Fossil Brown coal/Lignite": "FossilBrowncoalLignite",
+  "Fossil Coal-derived gas": "FossilCoalderivedgas",
+  "Fossil Gas": "FossilGas",
+  "Fossil Hard coal": "FossilHardcoal",
+  "Fossil Oil": "FossilOil",
+  "Fossil Oil shale": "FossilOilshale",
+  "Fossil Peat": "FossilPeat",
+  Geothermal: "Geothermal",
+  "Hydro Pumped Storage": "HydroPumpedStorage",
+  "Hydro Run-of-river and poundage": "HydroRunofriverandpoundage",
+  "Hydro Water Reservoir": "HydroWaterReservoir",
+  Marine: "Marine",
+  Nuclear: "Nuclear",
+  Other: "Other",
+  "Other renewable": "Otherrenewable",
+  Solar: "Solar",
+  Waste: "Waste",
+  "Wind Offshore": "WindOffshore",
+  "Wind Onshore": "WindOnshore",
+};
+const types = [
+  "Biomass",
+  "Fossil Brown coal/Lignite",
+  "Fossil Coal-derived gas",
+  "Fossil Gas",
+  "Fossil Hard coal",
+  "Fossil Oil",
+  "Fossil Oil shale",
+  "Fossil Peat",
+  "Geothermal",
+  "Hydro Pumped Storage",
+  "Hydro Run-of-river and poundage",
+  "Hydro Water Reservoir",
+  "Marine",
+  "Nuclear",
+  "Other",
+  "Other renewable",
+  "Solar",
+  "Waste",
+  "Wind Offshore",
+  "Wind Onshore",
+];
+const countries = ["Bulgaria", "Greece", "Italy", "North Macedonia", "Turkey"];
+const Ports = {
+  "Actual Total Load": "3001",
+  "Aggregate Generation per Type": "3002",
+  "Physical Flows": "3003",
+};
+//////////////////////////////////////////////////////////////////////////////////////////////
+function App() {
+  const [configObj, setConfigObj] = useState({
+    title: {
+      text: "Solar Employment Growth by Sector, 2010-2016",
+    },
+
+    subtitle: {
+      text: "Source: thesolarfoundation.com",
+    },
+
+    yAxis: {
+      title: {
+        text: "Number of Employees",
+      },
+    },
+
+    xAxis: {
+      accessibility: {
+        rangeDescription: "Range: 2010 to 2017",
+      },
+    },
+
+    legend: {
+      layout: "vertical",
+      align: "right",
+      verticalAlign: "middle",
+    },
+
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: false,
+        },
+        pointStart: 2010,
+      },
+    },
+
+    series: [
+      {
+        name: "Installation",
+        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+      },
+    ],
+
+    responsive: {
+      rules: [
+        {
+          condition: {
+            maxWidth: 500,
+          },
+          chartOptions: {
+            legend: {
+              layout: "horizontal",
+              align: "center",
+              verticalAlign: "bottom",
+            },
+          },
+        },
+      ],
+    },
+  });
+  const [mySeries, setMySeries] = useState([]);
+  const [myUrl, setMyUrl] = useState("http://localhost:3001/2022-01-01&GRCTY");
+  const [myDateString, setMyDateString] = useState("2022-01-01");
+  const [myQuantity, setMyQuantity] = useState(ATL);
+  const [myCountry, setMyCountry] = useState("GRCTY");
+  const [myCountry2, setMyCountry2] = useState("GRCTY");
+  const [myType, setMyType] = useState("Waste");
+  const [myTime, setMyTime] = useState(" no time yet");
+
+  const getMyTime = () => {
+    axios
+      .get(myTimeUrl)
+      .then((res) => {
+        setMyTime(res.data.date);
+        console.log("my time is: " + (Date.now() % 100000));
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {});
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      getMyTime();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    getMySeries();
+  }, [myTime]);
+
+  const getMySeries = () => {
+    axios
+      .get(myUrl)
+      .then((res) => {
+        let temp = DateStringArrayToEpoch(res.data);
+        temp = ArrayOfObjectsToArrayOfArrays(temp, ATL);
+        console.log("my series changed at", Date.now() % 10000);
+        setMySeries(temp);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        let tempConfig = { ...configObj };
+        tempConfig.series = [{ name: ATL, data: mySeries }];
+        setConfigObj(tempConfig);
+      });
+  };
+  const handleClick = async () => {
+    let newObj = { ...configObj };
+
+    let myDataArray = getMySeries();
+
+    newObj.series[0].data = myDataArray;
+    setConfigObj(newObj);
+  };
+
+  const getMyUrl = () => {
+    let tempUrl = "";
+    if (myQuantity === ATL) {
+      tempUrl =
+        "http://localhost:" +
+        Ports[myQuantity] +
+        "/" +
+        myDateString +
+        "&" +
+        CountriesObject[myCountry];
+    }
+
+    if (myQuantity === AGPT) {
+      tempUrl =
+        "http://localhost:" +
+        Ports[myQuantity] +
+        "/" +
+        myDateString +
+        "&" +
+        CountriesObject[myCountry] +
+        "&" +
+        typesObject[myType];
+    }
+
+    if (myQuantity === PF) {
+      tempUrl =
+        "http://localhost:" +
+        Ports[myQuantity] +
+        "/" +
+        myDateString +
+        "&" +
+        CountriesObject[myCountry] +
+        "&" +
+        CountriesObject[myCountry2];
+    }
+
+    setMyUrl(tempUrl);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>hello</p>
+        <p>welcome human , time is {myTime}:00</p>
+        <button onClick={getMyTime}>get the tiem from the cloud</button>
+        <MyChart configObj={configObj} />
+        <label>date now is {myDateString}</label>
+        insert MM-DD-YYYY or click little callendar
+        <input
+          type={"date"}
+          defaultValue={"2022-01-01"}
+          onChange={(event) => {
+            setMyDateString(event.target.value.slice(0, 10));
+          }}
+        ></input>
+        <label>quantity now is {Ports[myQuantity]}</label>
+        <select
+          onChange={(event) => {
+            setMyQuantity(event.target.value);
+          }}
+        >
+          <option value=" " selected></option>
+          {quantities.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        <label>country code now is {CountriesObject[myCountry]}</label>
+        <select
+          onChange={(event) => {
+            setMyCountry(event.target.value);
+          }}
+        >
+          <option value=" " selected></option>
+          {countries.map((item) => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))}
+        </select>
+        {myQuantity === AGPT ? (
+          <>
+            <label>Generation Type {typesObject[myType]}</label>
+            <select
+              onChange={(event) => {
+                setMyType(event.target.value);
+              }}
+            >
+              <option value=" " selected></option>
+              {types.map((item) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </>
+        ) : (
+          ""
+        )}
+        {myQuantity === PF ? (
+          <>
+            <label>country2 code now is {CountriesObject[myCountry2]}</label>
+            <select
+              onChange={(event) => {
+                setMyCountry2(event.target.value);
+              }}
+            >
+              <option value=" " selected></option>
+              {countries.map((item) => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </>
+        ) : (
+          " "
+        )}
+        <label>url now is {myUrl}</label>
+        <button onClick={getMyUrl}>update URL</button>
+        <button onClick={handleClick}>
+          click and wait a sec and click again to chage data
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+function DateStringArrayToEpoch(myJson) {
+  for (let i = 0; i < myJson.length; i++) {
+    myJson[i].DateTime = new Date(myJson[i].DateTime).getTime();
+  }
+  return myJson;
+}
+function ArrayOfObjectsToArrayOfArrays(myJson, parentQuantity) {
+  let myJsonArray = [];
+
+  if (parentQuantity === ATL) {
+    for (let i = 0; i < myJson.length; i++) {
+      myJsonArray.push([myJson[i].DateTime, myJson[i].TotalLoadValue]);
+    }
+  }
+  if (parentQuantity === AGPT) {
+    for (let i = 0; i < myJson.length; i++) {
+      myJsonArray.push([myJson[i].DateTime, myJson[i].ActualGenerationOutput]);
+    }
+  }
+  if (parentQuantity === PF) {
+    for (let i = 0; i < myJson.length; i++) {
+      myJsonArray.push([myJson[i].DateTime, myJson[i].FlowValue]);
+    }
+  }
+
+  return myJsonArray;
+}
